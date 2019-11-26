78b3850c6067e86e935cfe638af0b0bb2841716f
Andrew Jones <ascjones@gmail.com>
2019-11-26 18:39:02 +0000

    Flseh out example, not tested yet

--- contracts/custom_type/lib.rs
+++ contracts/custom_type/lib.rs
@@ -1,76 +1,86 @@
 #![feature(proc_macro_hygiene)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use ink_core::storage;
+use ink_core::memory::{
+    format,
+    vec::Vec,
+};
+
 use ink_lang2 as ink;
 
+/// Define hashing functions required for hashing the key to read a Value from runtime storage
+mod hashing {
+    /// Do a XX 128-bit hash and place result in `dest`.
+    pub fn twox_128_into(data: &[u8], dest: &mut [u8; 16]) {
+        use ::core::hash::Hasher;
+        let mut h0 = twox_hash::XxHash::with_seed(0);
+        let mut h1 = twox_hash::XxHash::with_seed(1);
+        h0.write(data);
+        h1.write(data);
+        let r0 = h0.finish();
+        let r1 = h1.finish();
+        use byteorder::{ByteOrder, LittleEndian};
+        LittleEndian::write_u64(&mut dest[0..8], r0);
+        LittleEndian::write_u64(&mut dest[8..16], r1);
+    }
+
+    /// Do a XX 128-bit hash and return result.
+    pub fn twox_128(data: &[u8]) -> [u8; 16] {
+        let mut r: [u8; 16] = [0; 16];
+        twox_128_into(data, &mut r);
+        r
+    }
+}
+
+/// Contract to demonstrate reading a custom struct directly from runtime storage
 #[ink::contract(version = "0.1.0")]
 mod custom_type {
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
-    struct CustomType {
-        /// Stores a single `bool` value on the storage.
-        value: storage::Value<bool>,
+    struct CustomRuntimeStorageTypeContract {}
+
+    /// Copy of the custom type defined in `/runtime/src/template.rs`.
+    ///
+    /// # Requirements
+    /// In order to decode a value of that type from the runtime storage:
+    ///   - The type must match exactly the custom type defined in the runtime
+    ///   - It must implement `Decode`, usually by deriving it as below
+    #[derive(scale::Decode, scale::Encode)]
+    pub struct Foo {
+        id: u32,
+        data: Vec<u8>,
     }
 
-    impl CustomType {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+    impl CustomRuntimeStorageTypeContract {
+        /// Constructor initializes the contract
         #[ink(constructor)]
-        fn new(&mut self, init_value: bool) {
-            self.value.set(init_value);
+        fn new(&mut self) {
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
+        /// Attempts to read an instance of the custom struct from runtime storage
         ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        fn default(&mut self) {
-            self.new(false)
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+        /// Returns `None` if the key does not exist, or it failed to decode the value.
         #[ink(message)]
-        fn flip(&mut self) {
-            *self.value = !self.get();
-        }
+        fn read_custom_runtime(&self) -> Option<Foo> {
+            // The raw key for a storage Value is `<ModuleName> <ValueName>`
+            let raw_key = b"TemplateModule FooStore";
+            // A storage Value key is hashed using `twox_128`
+            let hashed_key = hashing::twox_128(&raw_key[..]);
 
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        fn get(&self) -> bool {
-            *self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[test]
-        fn default_works() {
-            // Note that even though we defined our `#[ink(constructor)]`
-            // above as `&mut self` functions that return nothing we can call
-            // them in test code as if they were normal Rust constructors
-            // that take no `self` argument but return `Self`.
-            let custom_type = CustomType::default();
-            assert_eq!(custom_type.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-            let mut custom_type = CustomType::new(false);
-            assert_eq!(custom_type.get(), false);
-            custom_type.flip();
-            assert_eq!(custom_type.get(), true);
+            // Attempt to read and decode the value directly from the runtime storage
+            let result = self.env().get_runtime_storage::<Foo>(&hashed_key[..]);
+            match result {
+                Ok(foo) => {
+                    // Return the successfully decoded instance of `Foo`
+                    Some(foo)
+                },
+                Err(err) => {
+                    // Either the key did not exist or it failed to decode.
+                    // Print the reason for the error and return None.
+                    // *Note:* `println` should only be used for debugging, not in production contracts.
+                    self.env().println(&format!("Error reading runtime storage: {:?}", err));
+                    None
+                }
+            }
         }
     }
 }
